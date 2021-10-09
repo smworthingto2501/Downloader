815de3c270c6057e77c6892ed8b62ddbc5834c27
sarahworthington <smworthingto@wisc.edu>
2021-10-09 13:45:52 -0500

    progress

---
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private Button startButton;
+    private volatile boolean stopThread = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+    }
+
+    public void mockFileDownloader(){
+        runOnUiThread(new Runnable(){
+            @Override
+            public void run(){
+                startButton.setText("DOWNLOADING....");
+            }
+        });
+
+        for(int downloadProgress = 0; downloadProgress <= 100; downloadProgress=downloadProgress+10 ){
+            if (stopThread) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run(){
+                        startButton.setText("Start");
+                    }
+                });
+                return;
+            }
+
+            Log.d(TAG, "Download Progress: " + downloadProgress + "%");
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        runOnUiThread(new Runnable(){
+            @Override
+            public void run(){
+                startButton.setText("Start");
+            }
+        });
+    }
+
+    public void startDownload(View view){
+        mockFileDownloader();
+    }
+    public void stopDownload(View view){
+        stopThread = true;
+    }
+}
